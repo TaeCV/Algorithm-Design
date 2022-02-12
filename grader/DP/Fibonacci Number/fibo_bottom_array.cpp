840f6bcf5ca4df1d5b3079471c9c251f8a1499b1
Chayakorn Vongbunsin <taecv.work@gmail.com>
2022-02-12 09:48:01 +0700

    Create fibo_bottom_array.cpp

---
+++ grader/DP/Fibonacci Number/fibo_bottom_array.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int F[n + 2];
+    F[0] = 0;
+    F[1] = 1;
+    for (int i = 2; i <= n; ++i)
+        F[i] = F[i - 1] + F[i - 2];
+    cout << F[n];
+}
