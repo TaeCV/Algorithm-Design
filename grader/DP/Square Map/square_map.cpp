6a7d722dfd38ee568b429dca2562ac2390d7066b
Chayakorn Vongbunsin <taecv.work@gmail.com>
2022-02-21 14:32:05 +0700

    Create square_map.cpp

---
+++ grader/DP/Square Map/square_map.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int R, C, M[1005][1005];
+
+void process(int i, int j, int k) {
+}
+
+int main() {
+    cin >> R >> C;
+    for (int i = 1; i <= R; ++i) {
+        for (int j = 1; j <= C; ++j) {
+            cin >> M[i][j];
+        }
+    }
+    process(1, 1, 0);
+}
