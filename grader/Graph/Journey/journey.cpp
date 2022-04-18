3a1c52f2ff03f53b2646e9ee91c9a3cd368f4bd4
Chayakorn Vongbunsin <taecv.work@gmail.com>
2022-04-18 09:38:44 +0700

    Graph problems extra

---
+++ grader/Graph/Journey/journey.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    int M[N][N];
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < N; ++j) {
+            cin >> M[i][j];
+        }
+    }
+    int cnt = 0;
+}
